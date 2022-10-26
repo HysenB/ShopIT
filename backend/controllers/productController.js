fb0034d07982d2b50a1480cf683b5ea6ff6d2199
HysenB <hysi2000@live.com>
2022-10-26 09:47:21 +0200

    search, filter, pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,9 +2,11 @@ const Product = require('../models/product');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 // Create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+
     const product = await Product.create(req.body);
 
     res.status(201).json({
@@ -14,12 +16,26 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 })
 
 // Get all products => /api/v1/products
+// Get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-    const products = await Product.find();
+
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments();
+
+
+    const apiFeatures = 
+            new APIFeatures(Product.find(), req.query)
+            .search()
+            .filter()
+            .pagination(resPerPage);
+
+    // const products = await Product.find();
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
         count: products.length,
+        productCount,
         products
     })
 })
