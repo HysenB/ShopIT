f63b1cc646aa6f14d60db950efb21f59faee43b4
HysenB <hysi2000@live.com>
2022-10-27 15:46:00 +0200

    get and update user profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,6 +6,7 @@ const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
 
 const crypto = require('crypto');
+const { send } = require('process');
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async(req, res, next) => {
@@ -117,6 +118,52 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res)
 
+})
+
+// Get currently logged-in user details => /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
+
+// Update / Change password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    // Check previous user password
+    const isMatched = await user.comparePassword(req.body.oldPassword)
+    if(!isMatched){
+        return next(new ErrorHandler('Old password is incorrect', 400))
+    }
+
+    user.password = req.body.password;
+    await user.save();
+    
+    sendToken(user, 200, res);
+})
+
+// Update user profile => /api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    // TODO: Update avatar
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true
+    })
 })
 
 
