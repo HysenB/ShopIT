c1e3b414d2e742583b12aecd577c53a55a1012ad
HysenB <hysi2000@live.com>
2022-10-28 15:52:10 +0200

    get, update, delete all users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,11 +10,15 @@ const {
     resetPassword,
     getUserProfile,
     updatePassword,
-    updateProfile
+    updateProfile,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
 
 } = require('../controllers/authController');
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 
 
@@ -28,6 +32,12 @@ router.route('/logout').get(logout);
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
 router.route('/password/update').put(isAuthenticatedUser, updatePassword)
-router.route('/me/update').put(isAuthenticatedUser, updateProfile)
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id')
+.get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+.put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+.delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
